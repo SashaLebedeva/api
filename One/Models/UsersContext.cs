e0f218911251687c7c9d4de9723bd7080acad4da
swankaraswan <swankaraswan@outlook.com>
2019-02-08 23:52:14 +0600

    Add project files.

---
+++ One/Models/UsersContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace One.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        { }
+    }
+}
